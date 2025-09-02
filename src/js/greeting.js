8323769e2783d041d32343df9db8a10d7f59c6b5
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-09-02 12:29:26 +0200

    Prints name to browser

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -5,17 +5,35 @@ class Greeting {
   /**
    *
    */
-  setName(input) {
+  setName (input) {
     const name = input.value
-    this.convertToGreeting(name)
+    return this.showGreeting(name)
   }
 
   /**
    *
    */
-  convertToGreeting(name) {
+  showGreeting (name) {
     // Just printing the name to start with.
     console.log(name)
+    return `Hello ${name}`
+  }
+
+  /**
+   *
+   */
+  convertToRobberLanguage (name) {
+    const vowels = 'aeiouåäö'
+    // Stores the Robber Language name through the loop
+    const RLname = []
+
+    for (i in name) {
+      if (i.includes.vowel) {
+        RLname.add(i)
+      } else {
+        RLname.add(i + 'o' + i)
+      }
+    }
   }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,16 @@
 import Greeting from './greeting.js'
 
 // Collects the name from HTML form
-const input = document.querySelector("#name")
+const input = document.querySelector('#name')
 
 // New instance of Greeting class
 const greeting = new Greeting()
 
 // Listens for click on "OK"-button in HTML form
-const button = document.querySelector("#nameButton")
-button.addEventListener("click", (event) => {
-    console.log('Button clicked!')
-    greeting.setName(input)
+const button = document.querySelector('#nameButton')
+button.addEventListener('click', (event) => {
+  console.log('Button clicked!')
+  const message = greeting.setName(input)
+  const output = document.querySelector('.greetingBox')
+  output.textContent = message
 })
