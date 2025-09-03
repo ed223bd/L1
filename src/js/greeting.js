f665c4225854e81532d35375a6b191c61faf3bd6
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-09-03 19:02:18 +0200

    Shows robber language name

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -1,12 +1,13 @@
 /**
- *
+ * Class that takes a name and turns it into a robber language name.
+ * User is presented with a message containing the robber language name.
  */
 class Greeting {
   /**
    * Sets the name that the user typed in to the form.
    *
    * @param {string} input - The name the user gave.
-   * @returns
+   * @returns {string} The message in robber language.
    */
   setName (input) {
     const name = input.value
@@ -16,32 +17,40 @@ class Greeting {
   /**
    * Writes the greeting to the user.
    *
-   * @param name
-   * @returns
+   * @param {string} name - The set name.
+   * @returns {string} The complete message, using the robber language name.
    */
   showGreeting (name) {
     // Just printing the name to start with.
     console.log(name)
-    return `Hello ${name}`
+    // return `Hello ${name}`
+    const robberName = this.convertToRobberLanguage(name)
+    return `Hello! ${robberName}`
   }
 
   /**
    * Translates a string into robber language.
    *
    * @param {string} name - The set name.
+   * @returns {string} The converted robber language name.
    */
   convertToRobberLanguage (name) {
-    const vowels = 'aeiouåäö'
-    // Stores the Robber Language name through the loop
-    const RLname = []
+    console.log('convert called')
+    const isConsonants = 'bcdfghjklmnpqrstvwxz'
+    const isConsonantsCapital = 'BCDFGHJKLMNPQRSTVWXZ'
+    let newName = ''
 
-    for (i in name) {
-      if (i.includes.vowel) {
-        RLname.add(i)
+    name.split('').forEach(char => {
+      if (isConsonants.includes(char)) {
+        newName += (char + 'o' + char)
+      } else if (isConsonantsCapital.includes(char)) {
+        const smallChar = char.toLowerCase()
+        newName += (char + 'o' + smallChar)
       } else {
-        RLname.add(i + 'o' + i)
+        newName += (char)
       }
-    }
+    })
+    return newName
   }
 }
 
